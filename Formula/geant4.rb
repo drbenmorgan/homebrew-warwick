7ff56b33542cc9557590f74591c71becc1fe95eb
Ben Morgan <bmorgan.warwick@gmail.com>
2018-09-05 16:01:18 +0100

    geant4: remove obsolete patch data

--- Formula/geant4.rb
+++ Formula/geant4.rb
@@ -3,7 +3,6 @@ class Geant4 < Formula
   url "http://geant4.cern.ch/support/source/geant4.10.04.p02.tar.gz"
   version "10.04.02"
   sha256 "2cac09e799f2eb609a7f1e4082d3d9d3d4d9e1930a8c4f9ecdad72aad35cdf10"
-  patch :DATA
 
   depends_on "cmake" => :build
   depends_on "zlib"
@@ -40,43 +39,4 @@ class Geant4 < Formula
     system "false"
   end
 end
-
-__END__
-diff --git a/cmake/Modules/Geant4ConfigureConfigScript.cmake b/cmake/Modules/Geant4ConfigureConfigScript.cmake
-index 2a7a4cc..79a327e 100644
---- a/cmake/Modules/Geant4ConfigureConfigScript.cmake
-+++ b/cmake/Modules/Geant4ConfigureConfigScript.cmake
-@@ -117,7 +117,9 @@ if(NOT GEANT4_BUILD_GRANULAR_LIBS AND UNIX)
-     set(G4_BUILTWITH_GDML "yes")
-     set(G4_XERCESC_INCLUDE_DIRS ${XERCESC_INCLUDE_DIRS})
-     list(REMOVE_DUPLICATES G4_XERCESC_INCLUDE_DIRS)
--    list(REMOVE_ITEM G4_XERCESC_INCLUDE_DIRS ${_cxx_compiler_dirs})
-+    if(_cxx_compiler_dirs)
-+      list(REMOVE_ITEM G4_XERCESC_INCLUDE_DIRS ${_cxx_compiler_dirs})
-+    endif()
- 
-     set(G4_XERCESC_CFLAGS )
-     foreach(_dir ${G4_XERCESC_INCLUDE_DIRS})
-@@ -157,7 +159,9 @@ if(NOT GEANT4_BUILD_GRANULAR_LIBS AND UNIX)
-     endif()
- 
-     list(REMOVE_DUPLICATES G4_QT_INCLUDE_DIRS)
--    list(REMOVE_ITEM G4_QT_INCLUDE_DIRS ${_cxx_compiler_dirs})
-+    if(_cxx_compiler_dirs)
-+      list(REMOVE_ITEM G4_QT_INCLUDE_DIRS ${_cxx_compiler_dirs})
-+    endif()
- 
-     set(G4_QT_CFLAGS )
-     foreach(_dir ${G4_QT_INCLUDE_DIRS})
-@@ -218,7 +222,9 @@ if(NOT GEANT4_BUILD_GRANULAR_LIBS AND UNIX)
-   if(G4_CONFIG_NEEDS_X11)
-     set(_raw_x11_includes ${X11_INCLUDE_DIR})
-     list(REMOVE_DUPLICATES _raw_x11_includes)
--    list(REMOVE_ITEM _raw_x11_includes ${_cxx_compiler_dirs})
-+    if(_cxx_compiler_dirs)
-+      list(REMOVE_ITEM _raw_x11_includes ${_cxx_compiler_dirs})
-+    endif()
-     set(G4_X11_CFLAGS )
-     foreach(_p ${_raw_x11_includes})
-       set(G4_X11_CFLAGS "-I${_p} ${G4_X11_CFLAGS}")
 
